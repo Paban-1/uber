80091a4fa61d73bd37a553dcf8f6b97c291d9582
Paban-1 <rpaban89@gmail.com>
2026-04-25 19:09:36 +0530

    fix usermodel & usercontroller

--- models/user.model.js
+++ models/user.model.js
@@ -6,13 +6,15 @@ const jwt = require('jsonwebtoken')
 // Created User Model
 const userSchema = new mongoose.Schema({
     fullname: {
-        type: String,
-        require: true,
-        minlength: [3, 'First name must be at least 3 characters long']
-    },
-    lastname: {
-        type: String,
-        minlength: [3, 'Last name must be at least 3 characters long']
+        firstname: {
+            type: String,
+            require: true,
+            minlength: [3, 'First name must be at least 3 characters long']
+        },
+        lastname: {
+            type: String,
+            minlength: [3, 'Last name must be at least 3 characters long']
+        }
     },
     email: {
         type: String,
