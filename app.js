a26b32077424a4e0c819231e0f6ddfad02a8cc4f
Paban-1 <rpaban89@gmail.com>
2026-04-27 20:25:15 +0530

    added authmiddleware and userlogin

--- app.js
+++ app.js
@@ -6,11 +6,14 @@ const app = express()
 const connectToDb = require('./db/db')
 // require user route for config and use 
 const userRoutes = require('./routes/user.routes')
+// Require cookie parser for get the token from cookies
+const cookieParser = require('cookie-parser')
 
 connectToDb()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser()) 
 
 // Config the routes
 app.use('/users', userRoutes)
